9336621558363c4210b345ac03d2477ecff33613
Richard L. Jackson <denezt@yahoo.com>
2017-11-18 19:37:56 +0100

    Adding Examples - 2017-11-18 19:37:56

---
+++ Examples/JsonEncodeDemo1.java
@@ -0,0 +1,16 @@
+import org.json.simple.JSONObject;
+import java.io.*;
+
+class JsonEncodeDemo1 {
+
+   public static void main(String[] args){
+      JSONObject obj = new JSONObject();
+
+      obj.put("name", "foo");
+      obj.put("num", new Integer(100));
+      obj.put("balance", new Double(1000.21));
+      obj.put("is_vip", new Boolean(true));
+
+      System.out.print(obj);
+   }
+}
